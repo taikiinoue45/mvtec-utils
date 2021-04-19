a13e04078438f9a23648248874283375c8a902fc
TaikiInoue <taikiinoue45@gmail.com>
2021-04-19 06:29:14 +0000

    Add mvtec.dataset/

--- mvtec/dataset/mvtec.py
+++ mvtec/dataset/mvtec.py
@@ -1,15 +1,11 @@
-import os
-import tarfile
 from pathlib import Path
-from typing import Callable, List, Tuple, Union
-from urllib.request import urlretrieve
+from typing import List, Tuple, Union
 
 import cv2
 import pandas as pd
 from albumentations import Compose
 from torch import Tensor
 from torch.utils.data import Dataset
-from tqdm import tqdm
 
 
 class MVTecDataset(Dataset):
@@ -18,7 +14,6 @@ class MVTecDataset(Dataset):
         data_dir: Union[Path, str],
         queries: List[str],
         transforms: Compose,
-        download: bool = False,
     ) -> None:
 
         """PyTorch custom dataset for MVTec AD
@@ -31,23 +26,8 @@ class MVTecDataset(Dataset):
 
         self.data_dir = Path(data_dir)
         self.transforms = transforms
-        if download:
-            self.download()
-
         info_csv = pd.read_csv(self.data_dir / "info.csv")
         self.df = pd.concat([info_csv.query(q) for q in queries])
-
-    def download(self) -> None:
-
-        os.makedirs(self.data_dir, exist_ok=True)
-        base_url = "ftp://guest:GU.205dldo@ftp.softronics.ch/mvtec_anomaly_detection/"
-        filename = "mvtec_anomaly_detection.tar.xz"
-        url = base_url + filename
-        tarxz_path = self.data_dir / filename
-        urlretrieve(url, tarxz_path, reporthook=_gen_bar_updater())
-
-        with tarfile.open(tarxz_path, "r:xz") as tar:
-            tar.extractall(path=self.data_dir)
 
     def __getitem__(self, index: int) -> Tuple[str, Tensor, Tensor]:
 
@@ -62,21 +42,8 @@ class MVTecDataset(Dataset):
         mask[mask != 0] = 1
 
         data = self.transforms(image=img, mask=mask)
-
         return (stem, data["image"], data["mask"])
 
     def __len__(self) -> int:
 
         return len(self.df)
-
-
-def _gen_bar_updater() -> Callable[[int, int, int], None]:
-    pbar = tqdm(total=None)
-
-    def _bar_update(count: int, block_size: int, total_size: int) -> None:
-        if pbar.total is None and total_size:
-            pbar.total = total_size
-        progress_bytes = count * block_size
-        pbar.update(progress_bytes - pbar.n)
-
-    return _bar_update
